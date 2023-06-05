09c168f8b1da3c64dd17f5d45a4206db837d1ecc
Yale Araujo <yale850@gmail.com>
2023-06-05 20:31:31 -0300

    Revert "fix: trying fix"
    This reverts commit 22cca0f39d0de362164f9f7ae7b74ddaba675eab.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <body>{children}</body>
+      <body className="bg-[url('../assets/background-image.jpg')] bg-cover backdrop-blur-sm">
+        {children}
+      </body>
     </html>
   )
 }
