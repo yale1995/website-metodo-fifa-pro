84d5682cbb523d48e7e012ba1dcc184818b84cc3
Yale Araujo <yale850@gmail.com>
2023-06-04 21:17:05 -0300

    feat: metodo fifapro logo

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,10 @@
+export function Logo() {
+  return (
+    <p className="text-4xl text-center text-gray-300">
+      MÉTODO
+      <br />
+      <span className="font-bold text-5xl">FIFA</span>
+      <span className="text-green-600 font-bold text-5xl">PRO</span>
+    </p>
+  )
+}
