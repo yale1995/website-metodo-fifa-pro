b2f10f8486604ebe036bc752794ffacd53f34014
Yale Araujo <yale850@gmail.com>
2023-06-05 18:33:11 -0300

    feat: zoom when the user clicks in card

--- src/components/Carousel/CarouselCard.tsx
+++ src/components/Carousel/CarouselCard.tsx
@@ -1,14 +1,24 @@
 import Image from 'next/image'
+import { useState } from 'react'
 
 interface CarouselCardProps {
   pathImage: string
 }
 
 export function CarouselCard({ pathImage }: CarouselCardProps) {
+  const [onZoom, setOnZoom] = useState(false)
+
+  const handleCardZoom = () => {
+    setOnZoom((state) => !state)
+  }
+
   return (
     <div
-      className="shrink-0 snap-start rounded-lg overflow-hidden
-     hover:scale-150 transition-transform duration-300"
+      onClick={handleCardZoom}
+      className={`shrink-0 snap-start rounded-lg overflow-hidden transition-all duration-300 ${
+        onZoom ? 'absolute left-0 right-0 mx-auto transform scale-150' : ''
+      }`}
+      style={{ maxWidth: '180px' }}
     >
       <Image
         src={pathImage}
