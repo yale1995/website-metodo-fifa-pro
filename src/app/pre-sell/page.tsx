6f898e897b61a9dc010a631248981dc4d7c86b7b
Yale Araujo <yale850@gmail.com>
2023-06-04 21:16:29 -0300

    feat: pre sell page

--- src/app/pre-sell/page.tsx
+++ src/app/pre-sell/page.tsx
@@ -1,3 +1,23 @@
+'use client'
+
+import { Logo } from '@/components/Logo'
+import { QuizzInvitation } from '../../components/QuizzInvitation/QuizzInvitation'
+import { useState } from 'react'
+
 export default function PreSellPage() {
-  return <h1>Pre Sell Page</h1>
+  const [step] = useState(1)
+
+  function renderComponent() {
+    switch (step) {
+      case 1:
+        return <QuizzInvitation />
+    }
+  }
+
+  return (
+    <div className="flex flex-col gap-3 min-h-screen h-full justify-center bg-indigo-950/75 backdrop-brightness-75 px-2 py-4">
+      <Logo />
+      {renderComponent()}
+    </div>
+  )
 }
