c2541492c44ca0b3c47e116cc95580002c6d5ed0
Yale Araujo <yale850@gmail.com>
2023-06-06 17:09:25 -0300

    data: array of strings of carousel images

---
+++ src/data/carousel-images.ts
@@ -0,0 +1,12 @@
+export const carouselImages = [
+  '/carousel-images/1.jpeg',
+  '/carousel-images/2.jpeg',
+  '/carousel-images/3.jpeg',
+  '/carousel-images/4.jpeg',
+  '/carousel-images/5.jpeg',
+  '/carousel-images/1.jpeg',
+  '/carousel-images/2.jpeg',
+  '/carousel-images/3.jpeg',
+  '/carousel-images/4.jpeg',
+  '/carousel-images/5.jpeg',
+]
