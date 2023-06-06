e6ca394ded214c0867223c0f516d42d28a311524
Yale Araujo <yale850@gmail.com>
2023-06-06 18:06:49 -0300

    feat: component question three

--- src/components/QuestionThree/index.tsx
+++ src/components/QuestionThree/index.tsx
@@ -1,30 +1,39 @@
 import { LoadingBar } from '../LoadingBar'
+import { QuestionsProps } from '../QuestionOne'
 
-export function QuestionThree({ onAdvance }: any) {
+export function QuestionThree({ onAdvance }: QuestionsProps) {
   return (
     <div className="mx-auto flex flex-col items-center">
-      <LoadingBar progress={30} />
+      <LoadingBar progress={100} />
       <div className="text-center py-10">
-        <h1 className="text-4xl font-bold text-green-600">PERGUNTA 01</h1>
+        <h1 className="text-4xl font-bold text-green-600">PERGUNTA 03</h1>
         <h2 className="text-3xl font-light text-gray-300 py-2">
-          Você possui renda igual ou superior a R$ 5000?
+          Possui experiências com apostas esportivas?
         </h2>
 
         <div className="flex flex-col gap-4 max-w-xs mx-auto py-4">
           <button
             type="button"
-            className="bg-green-600 p-3 rounded-lg text-green-50 text-2xl font-bold hover:bg-green-500"
+            className="bg-green-600 p-3 rounded-lg text-gray-300 text-2xl font-bold hover:bg-green-500"
             onClick={onAdvance}
           >
-            SIM
+            Sim, ganhei dinheiro!
           </button>
 
           <button
             type="button"
-            className="bg-red-600 p-3 rounded-lg text-green-50 text-2xl font-bold hover:bg-red-500"
+            className="bg-red-600 p-3 rounded-lg text-gray-300 text-2xl font-bold hover:bg-green-500"
             onClick={onAdvance}
           >
-            Não
+            Sim, perdi dinheiro!
+          </button>
+
+          <button
+            type="button"
+            className="bg-yellow-600 p-3 rounded-lg text-gray-300 text-2xl font-bold hover:bg-green-500"
+            onClick={onAdvance}
+          >
+            Não tenho experiência!
           </button>
         </div>
       </div>
