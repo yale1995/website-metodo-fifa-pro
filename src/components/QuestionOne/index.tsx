e8eef4905bf2fdaf1da4e92e379221f2e12e1dcc
Yale Araujo <yale850@gmail.com>
2023-06-06 18:06:58 -0300

    feat: component question one

--- src/components/QuestionOne/index.tsx
+++ src/components/QuestionOne/index.tsx
@@ -1,9 +1,13 @@
 import { LoadingBar } from '../LoadingBar'
 
-export function QuestionOne({ onAdvance }: any) {
+export interface QuestionsProps {
+  onAdvance: () => void
+}
+
+export function QuestionOne({ onAdvance }: QuestionsProps) {
   return (
     <div className="mx-auto flex flex-col items-center">
-      <LoadingBar progress={30} />
+      <LoadingBar progress={33} />
       <div className="text-center py-10">
         <h1 className="text-4xl font-bold text-green-600">PERGUNTA 01</h1>
         <h2 className="text-3xl font-light text-gray-300 py-2">
@@ -13,7 +17,7 @@ export function QuestionOne({ onAdvance }: any) {
         <div className="flex flex-col gap-4 max-w-xs mx-auto py-4">
           <button
             type="button"
-            className="bg-green-600 p-3 rounded-lg text-green-50 text-2xl font-bold hover:bg-green-500"
+            className="bg-green-600 p-3 rounded-lg text-gray-300 text-2xl font-bold hover:bg-green-500"
             onClick={onAdvance}
           >
             SIM
@@ -21,7 +25,7 @@ export function QuestionOne({ onAdvance }: any) {
 
           <button
             type="button"
-            className="bg-red-600 p-3 rounded-lg text-green-50 text-2xl font-bold hover:bg-red-500"
+            className="bg-red-600 p-3 rounded-lg text-gray-300 text-2xl font-bold hover:bg-red-500"
             onClick={onAdvance}
           >
             Não
