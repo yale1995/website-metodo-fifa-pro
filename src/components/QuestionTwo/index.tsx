131bc3868b64526c57dbe35fd03345dd79523cee
Yale Araujo <yale850@gmail.com>
2023-06-06 18:06:35 -0300

    feat: component question two

--- src/components/QuestionTwo/index.tsx
+++ src/components/QuestionTwo/index.tsx
@@ -1,30 +1,39 @@
 import { LoadingBar } from '../LoadingBar'
+import { QuestionsProps } from '../QuestionOne'
 
-export function QuestionTwo({ onAdvance }: any) {
+export function QuestionTwo({ onAdvance }: QuestionsProps) {
   return (
     <div className="mx-auto flex flex-col items-center">
-      <LoadingBar progress={30} />
+      <LoadingBar progress={66} />
       <div className="text-center py-10">
-        <h1 className="text-4xl font-bold text-green-600">PERGUNTA 01</h1>
+        <h1 className="text-4xl font-bold text-green-600">PERGUNTA 02</h1>
         <h2 className="text-3xl font-light text-gray-300 py-2">
-          Você possui renda igual ou superior a R$ 5000?
+          Alguma vez já pensou em abrir seu próprio grupo de sinais?
         </h2>
 
         <div className="flex flex-col gap-4 max-w-xs mx-auto py-4">
           <button
             type="button"
-            className="bg-green-600 p-3 rounded-lg text-green-50 text-2xl font-bold hover:bg-green-500"
+            className="bg-green-600 p-3 rounded-lg text-gray-300 text-2xl font-bold hover:bg-green-500"
             onClick={onAdvance}
           >
-            SIM
+            Sim, tenho vontade!
           </button>
 
           <button
             type="button"
-            className="bg-red-600 p-3 rounded-lg text-green-50 text-2xl font-bold hover:bg-red-500"
+            className="bg-red-600 p-3 rounded-lg text-gray-300 text-2xl font-bold hover:bg-green-500"
             onClick={onAdvance}
           >
-            Não
+            Não, nunca pensei sobre!
+          </button>
+
+          <button
+            type="button"
+            className="bg-yellow-600 p-3 rounded-lg text-gray-300 text-2xl font-bold hover:bg-green-500"
+            onClick={onAdvance}
+          >
+            Não sei o que é isso!
           </button>
         </div>
       </div>
