1882abf8de1f9be03c22d6cd9c20efe2bb28b18f
Yale Araujo <yale850@gmail.com>
2023-06-06 17:10:40 -0300

    feat: modal component

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -6,7 +6,7 @@ interface ModalProps {
 
 export function Modal({ children }: ModalProps) {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 transition-all duration-700">
       {children}
     </div>
   )
