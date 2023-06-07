5e44fb590c5fdf5214168c85270d3cc2a9e44c34
Yale Araujo <yale850@gmail.com>
2023-06-07 10:50:46 -0300

    style: centering loading bar on desktop

--- src/components/LoadingBar/index.tsx
+++ src/components/LoadingBar/index.tsx
@@ -1,6 +1,6 @@
 export const LoadingBar = ({ progress }: any) => {
   return (
-    <div className="relative w-full h-10 bg-gray-200 rounded overflow-hidden md:w-96 mt-10">
+    <div className="relative mx-auto w-full h-10 bg-gray-200 rounded overflow-hidden md:w-96 mt-10">
       <div
         className="absolute top-0 left-0 h-full bg-yellow-500"
         style={{ width: `${progress}%` }}
