47e09a93eed5951e750f25b8f2718571d9983094
Yale Araujo <yale850@gmail.com>
2023-06-06 23:34:30 -0300

    style: changing colors

--- src/components/LoadingBar/index.tsx
+++ src/components/LoadingBar/index.tsx
@@ -2,11 +2,11 @@ export const LoadingBar = ({ progress }: any) => {
   return (
     <div className="relative w-full h-10 bg-gray-200 rounded overflow-hidden md:w-96 mt-10">
       <div
-        className="absolute top-0 left-0 h-full bg-gradient-to-r from-yellow-300 to-yellow-500"
+        className="absolute top-0 left-0 h-full bg-gradient-to-r from-fuchsia-500 to-fuchsia-900"
         style={{ width: `${progress}%` }}
       />
-      <div className="absolute top-0 left-0 w-full h-full bg-white bg-opacity-30"></div>
-      <div className="absolute top-0 left-0 w-full h-full bg-white bg-opacity-20"></div>
+      <div className="absolute top-0 left-0 w-full h-full bg-white bg-opacity-30" />
+      <div className="absolute top-0 left-0 w-full h-full bg-white bg-opacity-20" />
     </div>
   )
 }
