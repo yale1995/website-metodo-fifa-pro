5a1ab484a3f58e92505106c29336673ebbcd3246
Yale Araujo <yale850@gmail.com>
2023-06-07 10:31:35 -0300

    style: color bar to yellow

--- src/components/LoadingBar/index.tsx
+++ src/components/LoadingBar/index.tsx
@@ -2,7 +2,7 @@ export const LoadingBar = ({ progress }: any) => {
   return (
     <div className="relative w-full h-10 bg-gray-200 rounded overflow-hidden md:w-96 mt-10">
       <div
-        className="absolute top-0 left-0 h-full bg-gradient-to-r from-fuchsia-500 to-fuchsia-900"
+        className="absolute top-0 left-0 h-full bg-yellow-500"
         style={{ width: `${progress}%` }}
       />
       <div className="absolute top-0 left-0 w-full h-full bg-white bg-opacity-30" />
