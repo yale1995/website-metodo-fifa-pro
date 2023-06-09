aa561dce9bed89b6f493b663398077963d4d1ba9
Yale Araujo <yale850@gmail.com>
2023-06-09 10:41:07 -0300

    feat: created the user answer context

---
+++ src/contexts/UserAnswerContext.tsx
@@ -0,0 +1,23 @@
+import { ReactNode, createContext, useState } from 'react'
+
+interface UserAnswerContextProviderProps {
+  children: ReactNode
+}
+
+type UserAnswerContextType = {
+  userAnswer: string
+  setUserAnswer: (newState: string) => void
+}
+
+const UserAnswerContext = createContext({} as UserAnswerContextType)
+
+export function UserAnswerContextProvider({
+  children,
+}: UserAnswerContextProviderProps) {
+  const [userAnswer, setUserAnswer] = useState('')
+  return (
+    <UserAnswerContext.Provider value={{ userAnswer, setUserAnswer }}>
+      {children}
+    </UserAnswerContext.Provider>
+  )
+}
