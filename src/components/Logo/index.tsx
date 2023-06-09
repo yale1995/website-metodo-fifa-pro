079bbba1adacfdba21b1b2700befe785927d68a8
Yale Araujo <yale850@gmail.com>
2023-06-08 22:21:49 -0300

    style: removing padding from logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,6 +1,6 @@
 export function Logo() {
   return (
-    <p className="text-4xl text-center text-gray-300 p-2">
+    <p className="text-4xl text-center text-gray-300">
       MÉTODO
       <br />
       <span className="font-bold text-5xl">FIFA</span>
