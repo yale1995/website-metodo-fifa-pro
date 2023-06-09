e98a1965b7dd3f5cb46abe8b6bcbfd5a64465de6
Yale Araujo <yale850@gmail.com>
2023-06-08 21:22:25 -0300

    style: max width

--- src/components/LoadingBar/index.tsx
+++ src/components/LoadingBar/index.tsx
@@ -1,6 +1,6 @@
 export const LoadingBar = ({ progress }: any) => {
   return (
-    <div className="relative mx-auto w-full h-10 bg-gray-200 rounded overflow-hidden md:w-96 my-14">
+    <div className="relative mx-auto w-full h-10 bg-gray-200 rounded overflow-hidden md:max-w-lg my-14">
       <div
         className="absolute top-0 left-0 h-full bg-yellow-500"
         style={{ width: `${progress}%` }}
