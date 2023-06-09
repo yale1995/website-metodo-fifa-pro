37c64e814268713377ff8a1e429a3d0c11675631
Yale Araujo <yale850@gmail.com>
2023-06-08 21:36:20 -0300

    style: home page new style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Logo } from '@/components/Logo'
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-3 min-h-screen justify-center bg-indigo-950 md:bg-indigo-950/75 backdrop-brightness-75 px-2 py-4">
+    <div className="flex flex-col gap-3 min-h-screen bg-indigo-950 md:bg-indigo-950/75 backdrop-brightness-75 px-2 py-4">
       <Logo />
       <div className="max-w-screen-md w-full mx-auto rounded-lg overflow-hidden my-10">
         <iframe
