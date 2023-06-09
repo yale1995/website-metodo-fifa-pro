079145f0bbbeefb77243c55f94f2daf88678181c
Yale Araujo <yale850@gmail.com>
2023-06-08 21:34:11 -0300

    style: loading transition

--- src/components/LoadingBar/index.tsx
+++ src/components/LoadingBar/index.tsx
@@ -2,7 +2,7 @@ export const LoadingBar = ({ progress }: any) => {
   return (
     <div className="relative mx-auto w-full h-10 bg-gray-200 rounded overflow-hidden md:max-w-lg my-14">
       <div
-        className="absolute top-0 left-0 h-full bg-yellow-500"
+        className="absolute top-0 left-0 h-full bg-yellow-500 transition-all duration-700"
         style={{ width: `${progress}%` }}
       />
       <div className="absolute top-0 left-0 w-full h-full bg-white bg-opacity-30" />
