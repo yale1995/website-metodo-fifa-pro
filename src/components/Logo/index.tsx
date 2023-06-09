d7ba599427d4feb3fcdd6729887d8e5e202b6967
Yale Araujo <yale850@gmail.com>
2023-06-08 21:19:05 -0300

    style: adding padding into logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,6 +1,6 @@
 export function Logo() {
   return (
-    <p className="text-4xl text-center text-gray-300">
+    <p className="text-4xl text-center text-gray-300 p-2">
       MÉTODO
       <br />
       <span className="font-bold text-5xl">FIFA</span>
