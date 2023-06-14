9aa2451c675a977f9ad6b853ab9daeead252e7e7
Yale Araujo <yale850@gmail.com>
2023-06-14 10:33:22 -0300

    assets: prints images paths

--- src/data/carousel-images.ts
+++ src/data/carousel-images.ts
@@ -10,3 +10,16 @@ export const carouselImages = [
   '/carousel-images/4.jpeg',
   '/carousel-images/5.jpeg',
 ]
+
+export const printslImages = [
+  '/prints-images/print-1.jpg',
+  '/prints-images/print-2.png',
+  '/prints-images/print-3.png',
+  '/prints-images/print-4.png',
+  '/prints-images/print-5.png',
+  '/prints-images/print-1.jpg',
+  '/prints-images/print-2.png',
+  '/prints-images/print-3.png',
+  '/prints-images/print-4.png',
+  '/prints-images/print-5.png',
+]
