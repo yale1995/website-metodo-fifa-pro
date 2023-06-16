24b6bb4798d2319534383eeb792328bef14d2480
Yale Araujo <yale850@gmail.com>
2023-06-16 15:24:04 -0300

    config: remove message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,60 @@
+import { Betting } from '@/components/Betting'
 import { Logo } from '@/components/Logo'
 import { SocialValidation } from '@/components/SocialValidation'
 import { VideoValidation } from '@/components/VideoValidation'
 import { Footer } from '../components/Footer'
+import { Fragment } from 'react'
+import { FAQ } from '@/components/FAQ'
+import { Button } from '@/components/Button'
 
 export default function Home() {
-  console.log('Yure is gay')
-  return (
-    <div className="flex flex-col gap-3 min-h-screen bg-indigo-950 md:bg-indigo-950/75 backdrop-brightness-75 pt-4">
-      <Logo />
-      <div className="max-w-screen-lg p-2 flex-1 w-full mx-auto rounded-lg overflow-hidden">
-        <h1 className="text-gray-300 max-w-xl mx-auto text-center text-2xl font-bold py-4">
-          ASSISTA AO VÍDEO ABAIXO E DESCUBRA COMO{' '}
-          <span className="text-yellow-300">
-            CONQUISTAR LUCROS DE ATÉ R$ 40.000 MENSAIS
-          </span>
-        </h1>
-        <iframe
-          allowFullScreen
-          src="https://www.youtube.com/embed/XgferMHVaEA?autoplay=1"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          className="w-full md:h-[450px] h-96"
-        />
-        <button className="p-2 md:p-4 bg-green-600 rounded-2xl font-bold text-gray-300 text-2xl block mx-auto mt-16 animate-bounce">
-          QUERO ALCANÇAR LUCROS MENSAIS CONSISTENTES!
-        </button>
-      </div>
+  const videos = [
+    {
+      name: 'Ayrton Rafael',
+      description:
+        'Aluno de Goiânia/GO aplicando o Método FIFA PRO já obteve mais R$9.000 de rsultado.',
+      path: '/videos/video-1.mp4',
+    },
 
+    {
+      name: 'Miguel Goulart',
+      description:
+        'Aluno de Teresina/PI com o Método FIFA PRO já obteve mais R$18.000 de lucro.',
+      path: '/videos/video-2.mp4',
+    },
+  ]
+  return (
+    <Fragment>
+      <div className="md:bg-[url('../assets/background-image.jpg')] bg-cover backdrop-blur-sm">
+        <div className="flex flex-col gap-3 min-h-screen bg-indigo-950 md:bg-indigo-950/75 backdrop-brightness-75 pt-4">
+          <Logo />
+          <div className="max-w-screen-lg flex-1 w-full mx-auto rounded-lg overflow-hidden">
+            <h1 className="text-gray-300 max-w-xl mx-auto text-center text-2xl font-bold px-2">
+              ASSISTA AO VÍDEO ABAIXO E DESCUBRA COMO{' '}
+              <span className="text-yellow-400">
+                CONQUISTAR LUCROS DE ATÉ R$ 40.000 MENSAIS
+              </span>
+            </h1>
+            <iframe
+              allowFullScreen
+              src="https://www.youtube.com/embed/XgferMHVaEA?autoplay=1"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              className="w-full md:h-[450px] h-96"
+            />
+            <div className="px-2">
+              <Button />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Betting order={1} />
       <SocialValidation />
-      <VideoValidation />
+      {videos.map((video) => (
+        <VideoValidation key={video.name} {...video} />
+      ))}
+      <Betting order={2} />
+      <FAQ />
       <Footer />
-    </div>
+    </Fragment>
   )
 }
