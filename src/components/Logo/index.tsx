5a2296c82b9be56ea1676bd324dd342149d37035
Yale Araujo <yale850@gmail.com>
2023-06-16 15:24:29 -0300

    feat: logo to web and to mobile

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,4 +1,18 @@
-export function Logo() {
+interface LogoProps {
+  isFooter?: boolean
+}
+
+export function Logo({ isFooter = false }: LogoProps) {
+  if (isFooter) {
+    return (
+      <p className="text-2xl text-center text-gray-300">
+        MÉTODO
+        <br />
+        <span className="font-bold text-3xl">FIFA</span>
+        <span className="text-green-600 font-bold text-3xl">PRO</span>
+      </p>
+    )
+  }
   return (
     <p className="text-4xl text-center text-gray-300">
       MÉTODO
