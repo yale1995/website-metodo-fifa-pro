78e8711e0b9fb75f5373d273c98429580389534c
Yale Araujo <yale850@gmail.com>
2023-06-19 00:00:39 -0300

    feat: text font size and strong

--- src/app/obrigado/page.tsx
+++ src/app/obrigado/page.tsx
@@ -6,10 +6,12 @@ export default function SuccessfulPayment() {
       <div className="flex flex-col gap-3 min-h-screen bg-indigo-950 md:bg-indigo-950/75 backdrop-brightness-75 pt-4 text-gray-50 md:px-24 px-2 py-10">
         <Logo />
 
-        <div>
-          <h1>Pagamento Realizado com sucesso! 🚀</h1>
+        <div className="max-w-6xl mx-auto md:py-10 px-2 flex-1">
+          <h1 className="text-center md:text-4xl font-bold text-2xl py-4">
+            Pagamento Realizado com sucesso! 🚀
+          </h1>
 
-          <p>
+          <p className="text-sm text-center md:text-base md:text-left">
             Parabéns! Você acaba de garantir o conteúdo mais atualizado do
             Brasil sobre apostas esportivas em FIFA. Agora, confira seu email e
             siga corretamente as instruções para ter acesso ao nosso conteúdo.
