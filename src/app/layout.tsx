7f4a3cbb3560ed2f3f02bfb53b3bcb3c0ccb8c0a
Yale Araujo <yale850@gmail.com>
2023-06-28 22:31:23 -0300

    feat: tag conversion

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,6 +48,29 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="pt-BR">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '554174783349868');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=554174783349868&ev=PageView&noscript=1" />`,
+          }}
+        />
+      </head>
       <title>Mude de Vida com FIFA PRO</title>
       <body className={roboto.className}>
         <UserAnswerContextProvider>{children}</UserAnswerContextProvider>
